8577a07dc413591a17f5a8d724695279880490f7
biboslaw <pieprzyk.sebastian@gmail.com>
2018-02-09 18:25:37 +0100

    v0.3

--- script/main.js
+++ script/main.js
@@ -1,8 +1,26 @@
 function main (){
-    var a = document.querySelector('.iconRwd')
-    a.addEventListener('click', function(e){  
+    var hamburger = document.querySelector('.iconRwd')
+    hamburger.addEventListener('click', function(e){  
        changeDisplay(e)
-    })   
+    })
+    var a = document.querySelectorAll('.topnav a')
+    console.log(a)
+        for(var j=0; j<a.length; j++){
+            a[j].addEventListener('click', function(){
+                bla() 
+            })
+                               
+        }
+}
+
+function bla (){
+    for (var k = 0; k < a.length; k++) {
+                  if (k === a.length - 1) {
+                      a[k].classList.toggle('iconRwd2')
+                  } else {
+                      a[k].classList.toggle('topnavRwd')
+                  }
+              }
 }
                                
 function changeDisplay(e) {
